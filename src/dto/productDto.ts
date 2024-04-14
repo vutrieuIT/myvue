c761a39c3c2645fbfadeb9cfab2f616db0eb3e1d
TrieuVD <trieuvd@runsystem.net>
2024-04-14 23:41:00 +0700

    add product item view

---
+++ src/dto/productDto.ts
@@ -0,0 +1,9 @@
+interface ProductDto {
+  id?: number | null;
+  name: string;
+  price: number;
+  description: string;
+  category: string;
+}
+
+export { ProductDto };
--- src/views/ProductItemView.vue
+++ src/views/ProductItemView.vue
@@ -1,40 +1,63 @@
 <template>
   <div class="flex justify-content-center h-full">
-    <div class="form-container flex flex-column bg-primary m-2">
+    <div class="form-container flex flex-column bg-primary p-2 m-2">
       <p class="text-4xl mt-2 mb-5">đăng ký</p>
       <form class="flex-grow-1 relative">
         <div class="field">
-          <label class="w-full text-left" for="username">Username</label>
+          <label class="w-full text-left" for="name">name</label>
           <InputText class="w-full" />
         </div>
+
         <div class="field">
-          <label class="w-full text-left" for="password">Password</label>
-          <Password
-            class="w-full"
-            id="password-input"
-            :feedback="false"
-            toggleMask
-          />
+          <label class="w-full text-left" for="price">price</label>
+          <InputText class="w-full" />
         </div>
-        <button class="absolute bottom-0 right-0 mr-4 mb-2">Đăng ký</button>
+
+        <div class="field">
+          <label class="w-full text-left" for="category">Category</label>
+          <InputText class="w-full" />
+        </div>
+
+        <div class="field">
+          <label class="w-full text-left" for="description">description</label>
+          <InputText class="w-full" />
+        </div>
+        <button class="absolute bottom-0 right-0 mr-4 mb-2">tạo</button>
       </form>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import Password from "primevue/password";
+import { defineComponent, reactive } from "vue";
+// import Password from "primevue/password";
 import InputText from "primevue/inputtext";
+import { ProductDto } from "@/dto/ProductDto";
+import { PropType } from "vue";
 
 export default defineComponent({
   name: "ProductItemView",
   components: {
-    Password,
+    // Password,
     InputText,
   },
-  setup() {
-    return {};
+  props: {
+    product: {
+      type: Object as PropType<ProductDto>,
+      default: () => ({
+        id: null,
+        name: "",
+        price: 0,
+        description: "",
+        category: "",
+      }),
+    },
+  },
+  setup(props) {
+    console.log(props.product);
+
+    const productItem = reactive<ProductDto>(props.product);
+    return { productItem };
   },
 });
 </script>
