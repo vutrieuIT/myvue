b6493cc0d601848d1d899e3ff8feaf90ffad248a
TrieuVD <trieuvd@runsystem.net>
2024-04-14 23:03:31 +0700

    config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import PrimeVue from "primevue/config";
 
 import "primevue/resources/themes/saga-blue/theme.css";
 import "primevue/resources/primevue.min.css";
@@ -9,4 +10,5 @@ import "primeflex/primeflex.css";
 
 const app = createApp(App);
 app.use(router);
+app.use(PrimeVue);
 app.mount("#app");
